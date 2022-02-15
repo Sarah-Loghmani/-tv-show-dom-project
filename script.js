7064c33332ed14ad78c74aafb5274f276e857677
sarah loghmani <sarah.loghmani80@gmail.com>
2022-02-15 22:58:03 +0330

    2

--- script.js
+++ script.js
@@ -1,62 +1,52 @@
 const search = document.querySelector("#search"); //input
 // console.log(search);
 
-const getShow = async function () {
+const getShow = async function() {
+  const container = document.createElement("div"); //div
 
-  const container = document.createElement("div");//div
-
-  // API API API API API API API API API API API API
+  // ------------API API API API API API API API API------------
   const response = await axios("https://api.tvmaze.com/shows/82/episodes");
   const episodes = response.data;
   console.log(episodes);
-  // API API API API API API API API API API API API 
 
-  const getImgs = ()=>{
+  // --------------------created cards-------------------------
+    episodes.forEach((ep) => {
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      const figure = document.createElement("figure");
+      const card = document.createElement("div");
+      card.classList.add('card')
+      const p = document.createElement("p");
 
-      episodes.forEach((ep) => {
-        const img = document.createElement("img");
-        img.src = ep.image.medium;
-        container.appendChild(img);
-        document.body.appendChild(container);
-      });
-  };
-  getImgs()
+      img.src = ep.image.medium;
+      figcaption.innerText = ep.name;
+      p.textContent = ep.summary;
 
-  search.addEventListener("input", function (e) {
-    console.log(e.target.value);
-    
-    episodes.filter(ep=>{
-        ep.name.includes(e.target.value) || 
-        ep.summary.includes(e.target.value);
-        const img = document.createElement("img");
-        img.src = ep.image.medium;
-        container.appendChild(img);
-        document.body.appendChild(container);
-        
+      figure.append(img, figcaption);
+      card.append(figure, p);
+      container.appendChild(card);
+      container.classList.toggle("flex");
+      document.body.appendChild(container);
     });
 
-    //   if (container.children.length !== 0) {
-    //     for (let i = 0; i < container.children.length; i++) {
-    //       container.children[i].remove();
-    //     }
-    //   }
-    //   console.log(container.children);
-    
+  //-----------------search input---------------------
+  search.addEventListener("keyup", (e) => {
+    console.log(e.target.value);
+
+    const cards = document.querySelectorAll(".card");
+    console.log(cards);
+
+    let searchInput = e.target.value.toLowerCase();
+
+    cards.forEach((card) => {
+        if (!card.textContent.toLowerCase().includes(searchInput)) {
+          card.style.display = "none";
+        }else{
+          card.style.display = "block"
+        };
+    });
   });
+
 };
 getShow();
 
-//   console.log(container);
-
-// const getImage = (episodes, div) => {
-//     console.log(episodes);
-
-//     episodes.forEach((ep) => {
-
-//         if(ep.image){
-//             const img = document.createElement("img");
-//             img.src = ep.image.medium
-//             div.append(img)
-//         };
-//     });
-// };
